5f98befd8653aafa532c0602ddbdeb69ca1c6af7
testSystem <electronicstinkerer@gmail.com>
2019-09-20 18:11:47 -0400

    Changed tabs to spaces and added functions

--- btelib/btelib.cpp
+++ btelib/btelib.cpp
@@ -6,79 +6,86 @@
 
 // Constructor, get the pins to be used for the serial port
 btelib::btelib(byte RxPin, byte TxPin) {
-	// Stuff used when initalizing the object goes here
-	_RxPin = RxPin;
-	_TxPin = TxPin;
-	SoftwareSerial bteSerial(_RxPin, _TxPin); // Tell which pins we will be using
+    // Stuff used when initalizing the object goes here
+    _RxPin = RxPin;
+    _TxPin = TxPin;
+    SoftwareSerial bteSerial(_RxPin, _TxPin); // Tell which pins we will be using
 }
 
 // Initialize our object
 void btelib::begin(unsigned long baudRate) {
-	bteSerial->begin(9600);	// Start the serial port
+    bteSerial->begin(baudRate);	// Start the serial port
 }
 
 // Check to make sure the bluetooth module is ready
 bool btelib::areYouThere() {
-	bteSerial->println("AT");
-	
-	// Check to see if we got back "OK"
-	return waitForResponse("OK", false, true);
+    bteSerial->println("AT");
+
+    // Check to see if we got back "OK"
+    return waitForResponse("OK", false, true);
 }
 
 // Set the timeout delay to X mS
 void btelib::setTimeout(unsigned long timeoutDelayWanted) {
-	btelib::timeoutDelay = timeoutDelayWanted;
+    btelib::timeoutDelay = timeoutDelayWanted;
 }
 
 unsigned long btelib::getTimeout() {
-	return timeoutDelay;
+    return timeoutDelay;
 }
 
 bool btelib::waitForResponse(String messageToWaitFor, bool caseSensitive, bool useTimeout) {
 	
-	unsigned long startTime = millis();	// Used for determining the timeout if needed.
+    unsigned long startTime = millis();	// Used for determining the timeout if needed.
 
-	char currentChar = ' ';
+    char currentChar = ' ';
 
-	// Get the length of our string
-	int stringLength = messageToWaitFor.length();
+    // Get the length of our string
+    int stringLength = messageToWaitFor.length();
 	
-	if (!caseSensitive)
-		messageToWaitFor.toUpperCase();
+    if (!caseSensitive)
+        messageToWaitFor.toUpperCase();
 	
-	int j = 0;	// The current number of correct characters in a row recieved
-	while (j != stringLength) {
+    int j = 0;	// The current number of correct characters in a row recieved
+    while (j != stringLength) {
 		
-		do { 
-			currentChar = bteSerial->read();
+        do { 
+            currentChar = btelib::readChar();
 			
-			// Now make sure that we do not run over the timeout delay if we are using it
-			if (useTimeout && ((millis() - startTime) >= timeoutDelay) )
-				return false;	//Hit the timeout, return false
-		} while (currentChar == -1);
+            // Now make sure that we do not run over the timeout delay if we are using it
+            if (useTimeout && ((millis() - startTime) >= timeoutDelay) )
+                return false;	//Hit the timeout, return false
+        } while (currentChar == \x00);
 		
-		// If we don't case about the case, ignore it.
-		if (!caseSensitive)
-			currentChar & 0x5F;
+        // If we don't case about the case, ignore it.
+        if (!caseSensitive)
+            currentChar & 0x5F;
 		
-		// If the current character that we are looking at is the next one in the string that 
-		//   we are waiting for, incriment the value, otherwise reset it.
-		if (currentChar == messageToWaitFor[j])
-			j++;
-		else 
-			j = 0;
-	}
+        // If the current character that we are looking at is the next one in the string that 
+        //   we are waiting for, incriment the value, otherwise reset it.
+        if (currentChar == messageToWaitFor[j])
+            j++;
+        else 
+            j = 0;
+    }
 	
-	// No timeout, "YAY" - return true
-	return true;
+    // No timeout, "YAY" - return true
+    return true;
 }
 
+// Get a character from the module
 char btelib::readChar() {
     char data = bteSerial->read(); 
-    
-    if (data == -1) {
-        data = '\x00';
-    }
-    
-    return data;
+   
+    return (data == -1) ? \x00 : data;
+}
+
+// Send a character to the module
+void btelib::writeChar(char characterToSend) {
+    bteSerial->print(char);
+}
+
+// Send a string to the module
+void btelib::writeln(String characterString, bool waitForOK) {
+    bteSerial->println(characterString);
 }
--- btelib/btelib.h
+++ btelib/btelib.h
@@ -7,50 +7,52 @@
 
 // For arduino standard libraries
 #if ARDUINO >= 100
-	#include "Arduino.h"
+    #include "Arduino.h"
 #else
-	#include "WProgram.h"
-	#include "pins_arduino.h"
-	#include "WConstants.h"
+    #include "WProgram.h"
+    #include "pins_arduino.h"
+    #include "WConstants.h"
 #endif
 
 class SoftwareSerial;	// forward declaration for the serial library
 
 // Here is the class for the driver functions
 class btelib {
-	public:
-		// Constructor
-		btelib(byte RxPin=2, byte TxPin=3);
+    public:
+        // Constructor
+        btelib(byte RxPin=2, byte TxPin=3);
 
 
-		// Methods
-		void btelib::begin(unsigned long baudRate);	// Setup BAUD rate
-		bool areYouThere(); // Get status (true = active)
+        // Methods
+        void btelib::begin(unsigned long baudRate);	// Setup BAUD rate
+        bool areYouThere(); // Get status (true = active)
 		
-		void setTimeout(unsigned long timeoutDelayWanted);	// Timeout for waiting for response from module
-		unsigned long getTimeout();	// Returns the timeout for the module command functions
+        void setTimeout(unsigned long timeoutDelayWanted);	// Timeout for waiting for response from module
+        unsigned long getTimeout();	// Returns the timeout for the module command functions
 		
-		void resetModule();	// Reset the module
-		String getFirmVer();	// Returns the firmware version of the module
-		void restoreDefault();	// Reset the module to factory defaults
-		String getModuleAddress();	// Returns the address of the module
-		bool setModuleName(String moduleName);	// Set the name of the module
-		String getModuleName();	// Get the name of the module
-		String getBlueDeviceName();	// Get the bluetooth device name
-		bool setModuleMode();	// Set the module mode 1=Master, 0=Slave, 2=Slave=Loop
-		byte getModuleMode();	// Gets the module mode (see setModuleMode())
+        void resetModule();	// Reset the module
+        String getFirmVer();	// Returns the firmware version of the module
+//        void restoreDefault();	// Reset the module to factory defaults
+//        String getModuleAddress();	// Returns the address of the module
+        bool setModuleName(String moduleName);	// Set the name of the module
+//        String getModuleName();	// Get the name of the module
+//        String getBlueDeviceName();	// Get the bluetooth device name
+        bool setModuleMode();	// Set the module mode 1=Master, 0=Slave, 2=Slave=Loop
+//        byte getModuleMode();	// Gets the module mode (see setModuleMode())
 
-        char readChar();
+        char readChar();    // Read a character from the module
+        void writeChar(char characterToSend);   // Write a single character to the module
+        void writeln(String characterString, bool waitForOK);   // Write line of characters to the module
 
-	private:
-		byte _RxPin;	// Pins used for the serial port
-		byte _TxPin;
-		SoftwareSerial *bteSerial;	// Name of serial port object
+    private:
+        byte _RxPin;	// Pins used for the serial port
+        byte _TxPin;
+        SoftwareSerial *bteSerial;	// Name of serial port object
 		
-		// Timeout delay for messages over the serial port
-		unsigned long timeoutDelay = 500;
+        // Timeout delay for messages over the serial port
+        unsigned long timeoutDelay = 500;
 		
-		bool waitForResponse(String messageToWaitFor, bool caseSensitive, bool useTimeout);
+        bool waitForResponse(String messageToWaitFor, bool caseSensitive, bool useTimeout);
 };
 
 #include "btelib.cpp"

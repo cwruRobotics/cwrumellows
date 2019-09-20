94af55e8c4bf98894df88a4a6b5f5b09fe7ea4d6
Matthias <MatthiasSaihttam1@gmail.com>
2019-09-20 17:37:52 -0400

    Begin adding button logic to Ardino file

--- cwrumellows.ino
+++ cwrumellows.ino
@@ -1,4 +1,4 @@
-#include "btelib.h"
+#include "btelib/btelib.h"
 
 btelib *bluetoothConnection;
 
@@ -6,12 +6,24 @@ void setup() {
     btelib bluetoothConnection(2, 3); //Rx pin, Tx pin
 }
 
-void loop() {
-    char input = bluetoothConnection->getChar();
+const enum BUTTONS {
+    TOP = 'A',
+    RIGHT,
+    DOWN,
+    LEFT,
+    TRIANGLE,
+    CIRCLE,
+    CROSS,
+    SQUARE
+}
 
-    switch (input){
-        case '\x00':
-            //All buttons are released
-            break;
+void loop() {
+    char input = bluetoothConnection->readChar();
+    
+    if (input == '\x00'){
+        //All buttons are released
+        //Set all buttons to false
+    }else {
+        
     }
 }

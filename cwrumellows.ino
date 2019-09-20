a83a2fbed14a2bb84e4e0d12f1ea7164d9f3acfd
Matthias <MatthiasSaihttam1@gmail.com>
2019-09-20 16:10:29 -0400

    Start adding Arduino code

--- cwrumellows.ino
+++ cwrumellows.ino
@@ -1,9 +1,17 @@
-#include "btelib/btelib.h"
+#include "btelib.h"
+
+btelib *bluetoothConnection;
 
 void setup() {
-    btelib(2, 3); //Rx pin, Tx pin
+    btelib bluetoothConnection(2, 3); //Rx pin, Tx pin
 }
 
 void loop() {
-    
+    char input = bluetoothConnection->getChar();
+
+    switch (input){
+        case '\x00':
+            //All buttons are released
+            break;
+    }
 }

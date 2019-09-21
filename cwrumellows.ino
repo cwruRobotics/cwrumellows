6450898bb456c88397126ec105cf818e5dd60ede
Matthias <MatthiasSaihttam1@gmail.com>
2019-09-21 15:55:20 -0400

    Logic for tracking left/right button presses

--- cwrumellows.ino
+++ cwrumellows.ino
@@ -1,12 +1,12 @@
-#include "btelib/btelib.h"
+//#include "btelib/btelib.h"
 
-btelib *bluetoothConnection;
+//btelib *bluetoothConnection;
 
 void setup() {
-    btelib bluetoothConnection(2, 3); //Rx pin, Tx pin
+//    btelib bluetoothConnection(2, 3); //Rx pin, Tx pin
 }
-/*
-const enum BUTTONS {
+
+enum BUTTONS {
     TOP = 'A',
     RIGHT,
     DOWN,
@@ -15,16 +15,45 @@ const enum BUTTONS {
     CIRCLE,
     CROSS,
     SQUARE
-}
+};
+
+bool turningRight = false;
+bool turningLeft = false;
 
 void loop() {
-    char input = bluetoothConnection->readChar();
-    
+//    char input = bluetoothConnection->readChar();
+    char input = 'h';
+
     if (input == '\x00'){
-        //All buttons are released
+        //One button are released
         //Set all buttons to false
+        //    If buttons are still pressed, we'll get a lower case letter soon and turn it back on
+        turningRight = false;
+        turningLeft = false;
     }else {
-        
+        if (isLowerCase(input)) {
+            input = input - 0x20;
+        }
+
+        switch(input) {
+            case LEFT:
+                turningLeft = true;
+                break;
+            case RIGHT: 
+                turningRight = true;
+                break;
+            default:
+                //Ignore button presses right now
+                break;
+        }
+
+        //If left and right are both pressed, pretend like neither is pressed
+        if (turningLeft && turningRight) {
+            turningRight = false;
+            turningLeft = false;
+        }
     }
+
+    //TODO:
+    //if left, turn turntable left, etc.
 }
-*/
